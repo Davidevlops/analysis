b4cbd84f538caecb90ff92098d9cdff1ba2681f8
David05 <davidgreendevlops@gmail.com>
2023-12-24 15:21:59 +0100

    play

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ combined_data = pd.concat([df_y,df_X], ignore_index=True )
 
 missing_values = combined_data.isnull().sum()
 print("missing_values",missing_values)
-if missing_values.any():
+# if missing_values.any():
     combined_data.fillna(combined_data.mean(), inplace=True)
     combined_data.dropna(inplace=True)
