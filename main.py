1dbc07282184dcc6c0946574929115a217557d6f
David05 <davidgreendevlops@gmail.com>
2023-12-24 15:12:16 +0100

    play

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import statsmodels.api as sm
 import patsy
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 # Load data from Excel files
-# df_y = pd.read_csv('./sect3_health.csv',low_memory=False)
+df_y = pd.read_csv('./sect3_health.csv',low_memory=False)
 df_X = pd.read_csv('./sect13_income.csv')
 
 
